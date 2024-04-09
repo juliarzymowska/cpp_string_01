177220f5d4b5fcc512ad383708272b2120db73e5
Julia <julia.rzymowska@student.uj.edu.pl>
2024-04-09 20:50:31 +0200

    string_01.h/.cc bez testu bielsko-biala

--- string_01.cc
+++ string_01.cc
@@ -3,18 +3,15 @@ using namespace string_toys;
 using std::cin;
 using std::cout;
 auto main() -> int { 
-    std::string a, b, pom; // glowny string, substring i pomocniczy do wczytywania a
-    /*while(std::getline(cin, a))
-    {
-        pom+=a;
-    }*/
+    std::string a, b; // glowny string, substring i pomocniczy do wczytywania a
+    
     std::getline(cin,a);
-    std::getline(cin, b);
+    std::getline(cin,b);
 
-    cout << reverse(pom) << "\n";
-    cout << first_to_upper(pom) << "\n";
-    cout << count_vowels(pom) << "\n";
-    cout << sum_digits(pom) << "\n";
-    cout << search_substr(pom, b) << "\n";
-    cout << custom_serach(pom.c_str(), b.c_str()) << "\n";
+    cout << reverse(a) << "\n";
+    cout << first_to_upper(a) << "\n";
+    cout << count_vowels(a) << "\n";
+    cout << sum_digits(a) << "\n";
+    cout << search_substr(a, b) << "\n";
+    cout << custom_serach(a.c_str(), b.c_str()) << "\n";
     return 0; }
--- string_01.h
+++ string_01.h
@@ -2,6 +2,7 @@
 #define CPP_STRING_H
 
 #include <iostream>
+#include <cmath>
 namespace string_toys {
 
 auto reverse(const std::string& str) -> std::string {
@@ -13,14 +14,89 @@ auto reverse(const std::string& str) -> std::string {
 }
 
 auto first_to_upper(const std::string& str) -> std::string {
-    std::string helper_variable;
-        for (int i = 0; i < str.size(); i++)
-        {
-            if (i == 0 || str[i - 1] == ' ' || str[i-1]=='\n')
-                helper_variable += std::toupper(str[i]);
+        bool ok=0;//czy to co jest przed str[i] jest znakiem
+        std::string helper_variable;
+        int ascii[18]={165, 198, 202, 163, 209, 211, 140, 143, 175, 185, 230, 234, 179, 241, 243, 156, 159, 191}; //tablica z polskimi znakami ascii do sprawdzenia
+        int str_int_value = fabs(static_cast <int> (str[0]));
 
-            else
-                helper_variable += str[i];
+        if(str_int_value == 185)
+            helper_variable+=(char)165;
+        else if (str_int_value == 230)
+            helper_variable +=(char)198;
+        else if (str_int_value == 234)
+            helper_variable += (char)202;
+        else if (str_int_value == 179)
+            helper_variable+=(char)163;
+        else if (str_int_value == 241)
+            helper_variable += (char)209;
+        else if (str_int_value == 243)   
+            helper_variable += (char)211;
+        else if (str_int_value == 156)
+            helper_variable += (char)140;
+        else if (str_int_value == 159)
+            helper_variable +=(char)143;
+        else if (str_int_value == 191)
+            helper_variable += (char)175; 
+        else if (str_int_value >= 97 && str_int_value <=122)
+            helper_variable += std::toupper(str[0]);
+        else helper_variable += (char)str_int_value;
+        
+        for (int i = 1; i <= str.size() -1; i++)
+        {
+            str_int_value = fabs(static_cast<int>(str[i-1]));
+
+            for(int j=0; j<18; j++)
+            {
+                if(str_int_value == ascii[j])
+                {
+                    ok=1;
+                    break;
+                }
+            }
+            if(!ok)
+                for(int j=65;j<=90;j++)
+                {
+                    if(str_int_value == (char)j)
+                    {
+                        ok=1;
+                        break;
+                    }
+                }
+            if(!ok)
+                for(int j=97;j<=122;j++)
+                {
+                    if(str_int_value == (char)j)
+                    {
+                        ok=1; 
+                        break;
+                    }
+                }
+
+            if(ok) helper_variable += str[i];
+            else 
+            {
+                if(str_int_value == 185)
+                    helper_variable+=(char)165;
+                else if (str_int_value == 230)
+                    helper_variable +=(char)198;
+                else if (str_int_value == 234)
+                    helper_variable += (char)202;
+                else if (str_int_value == 179)
+                    helper_variable+=(char)163;
+                else if (str_int_value == 241)
+                    helper_variable += (char)209;
+                else if (str_int_value == 243)   
+                    helper_variable += (char)211;
+                else if (str_int_value == 156)
+                    helper_variable += (char)140;
+                else if (str_int_value == 159)
+                    helper_variable +=(char)143;
+                else if (str_int_value == 191)
+                    helper_variable += (char)175; 
+                else helper_variable += std::toupper(str[i]);
+            }
+
+            ok=0;
         }
 
         return helper_variable;
